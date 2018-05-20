ca338e576c235283a7cd2882de3e3fe3b3289153
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2018-05-20 18:31:18 +0300

    Added MongoDb Database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var users = [];
+var router = express.Router();
+var mongodb = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
 
 
 app.get('/', function(req, res){
@@ -11,20 +13,44 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('new_user', function(user){
-    users[socket.id] = {id : socket.id, name : user};
-    socket.broadcast.emit('new_user',user,users);
+    mongodb.connect(url, function(err,db){
+      if(err) throw err;
+      dbo = db.db('chatdb');
+      dbo.collection("users").insertOne({id:socket.id,name:user}, function(err,res){
+        if (err) {
+          throw err;
+        }
+        db.close();
+      });
+    });
+    socket.broadcast.emit('new_user',user);
     console.log(user+' cevrimici oldu!\nid : '+socket.id+'\n');
   });
   socket.on('disconnect', function(){
-    console.log(users[socket.id].name+' cevrimdisi oldu!');
-    socket.broadcast.emit('user_gone',users[socket.id].name,users);
-    delete users[socket.id];
+    mongodb.connect(url, function(err,db){
+      if(err) throw err;
+      dbo = db.db('chatdb');
+      dbo.collection("users").findOne({id:socket.id}, function(err,res){
+        if (err) {
+          throw err;
+        }
+        console.log(res.name+' cevrimdisi oldu!');
+        socket.broadcast.emit('user_gone',res.name);
+        db.close();
+      });
+    });
+    mongodb.connect(url, function(err,db){
+      if(err) throw err;
+      dbo = db.db('chatdb');
+      dbo.collection("users").deleteOne({id:socket.id});
+      db.close();
+    });
   });
   socket.on('new_message', function(msg){
     io.emit('new_message',msg);
   });
 });
 
-http.listen(8000,function(){
-  console.log('Listening on *:8000');
+http.listen(3000,function(){
+  console.log('Listening on *:3000');
 });
