1d39217c5f6dc11b96f8e4c230c081b76bb961ad
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2018-05-22 20:38:36 +0300

    Changed deleting method

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var io = require('socket.io')(http);
 var router = express.Router();
 const database = require('./modules/database');
 
+app.use('/scripts/izitoast', express.static(__dirname + '/node_modules/izitoast/dist/js'));
+app.use('/styles/izitoast', express.static(__dirname + '/node_modules/izitoast/dist/css'));
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
 });
@@ -16,16 +20,20 @@ io.on('connection', function(socket){
     database.add_user(socket.id,user);
     socket.broadcast.emit('new_user',user);
     console.log(user+' cevrimici oldu!\nid : '+socket.id+'\n');
+    database.update_list(function(err){
+      if (err) {
+        throw err;
+      }
+    });
   });
   socket.on('disconnect', function(){
-    database.find_user(socket.id, function(err,res){
+    database.delete_user(socket.id, function(err,res){
       if (err) {
         throw err;
       }
       console.log(res+' cevrimdisi oldu!');
       socket.broadcast.emit('user_gone',res);
     });
-    database.delete_user(socket.id);
   });
   socket.on('new_message', function(msg){
     console.log(msg);
@@ -33,6 +41,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000,function(){
+http.listen(3000, function(){
   console.log('Listening on *:3000');
 });
