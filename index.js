029e0d6daa1243ff0deea568b28ee60b757988f3
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2018-05-24 05:12:09 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var router = express.Router();
 const database = require('./modules/database');
+var new_uc;
+var dlt_uc;
+var new_msg;
 
 app.use('/scripts/izitoast', express.static(__dirname + '/node_modules/izitoast/dist/js'));
 app.use('/styles/izitoast', express.static(__dirname + '/node_modules/izitoast/dist/css'));
@@ -17,27 +20,31 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('new_user', function(user){
-    database.add_user(socket.id,user);
-    socket.broadcast.emit('new_user',user);
-    console.log(user+' cevrimici oldu!\nid : '+socket.id+'\n');
-    database.update_list(function(err){
-      if (err) {
-        throw err;
-      }
-    });
+    new_uc = user.split(",");
+    socket.join(new_uc[1]);
+    database.add_user(socket.id,new_uc[0],new_uc[1]);
+    socket.in(new_uc[1]).emit('new_user',user);
+    console.log(new_uc[0]+' '+new_uc[1]+' adli kanalda cevrimici oldu!\nid : '+socket.id+'\n');
   });
   socket.on('disconnect', function(){
     database.delete_user(socket.id, function(err,res){
       if (err) {
         throw err;
       }
-      console.log(res+' cevrimdisi oldu!');
-      socket.broadcast.emit('user_gone',res);
+      if (res==404) {
+        console.log("isimsiz çıkış yapıldı!");
+      }
+      else{
+        dlt_uc = res.split(",");
+        console.log(dlt_uc[0]+' cevrimdisi oldu!');
+        io.in(dlt_uc[1]).emit('user_gone',dlt_uc[0]);
+      }
     });
   });
   socket.on('new_message', function(msg){
+    new_msg = msg.split("<^>");
     console.log(msg);
-    io.emit('new_message',msg+","+socket.id);
+    io.in(new_msg[1]).emit('new_message',new_msg[0]+","+socket.id);
   });
 });
 
--- modules/database.js
+++ modules/database.js
@@ -1,13 +1,14 @@
 var mongodb = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 var dbo;
+var ucidp;
 
 module.exports = {
-  add_user : function(uid,username){
+  add_user : function(uid,username,ch){
     mongodb.connect(url, function(err,db){
       if(err) throw err;
       dbo = db.db('chatdb');
-      dbo.collection("users").insertOne({id:uid,name:username}, function(err,res){
+      dbo.collection("users").insertOne({id:uid,name:username,channel:ch}, function(err,res){
         if (err) {
           throw err;
         }
@@ -21,36 +22,38 @@ module.exports = {
       dbo = db.db('chatdb');
       dbo.collection("users").findOneAndDelete({id:uid}, function(err,res){
         if (err) {
-              throw err;
-            }
-            if (!res) {
+              console.log("isimsiz çıkış yapıldı!");
+        }
+        if (!res.value) {
               callback(null, 404);
             }
-           else callback(null, res.value.name);
-      });
-      db.close();
-    });
-  },
-  update_list : function(callback){
-    mongodb.connect(url, function(err,db){
-      if (err) {
-        throw err;
-      }
-      dbo = db.db('chatdb');
-      dbo.collection("users").find({}).toArray(function(err,res){
-        if (err) {
-          throw err;
-        }
-        if (!res) {
-          console.log(404);
-        }
-        else {
-          res.forEach(function(element){
-          console.log(element.name);
-          });
-        }
+           else {
+             callback(null, res.value.name+","+res.value.channel);
+           }
       });
       db.close();
     });
   }
+  // update_list : function(callback){
+  //   mongodb.connect(url, function(err,db){
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     dbo = db.db('chatdb');
+  //     dbo.collection("users").find({}).toArray(function(err,res){
+  //       if (err) {
+  //         throw err;
+  //       }
+  //       if (!res) {
+  //         console.log(404);
+  //       }
+  //       else {
+  //         res.forEach(function(element){
+  //         console.log(element.name);
+  //         });
+  //       }
+  //     });
+  //     db.close();
+  //   });
+  // }
 }
